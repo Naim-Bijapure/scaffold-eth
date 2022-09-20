dc71216b82266ae77d49acc95a46b5605b844f4d
NaimBijapure <naim@vinnovatetechnologies.com>
2022-09-20 18:45:04 +0530

    Title:bug:gelato extend contract

--- packages/hardhat/contracts/YourNFT.sol
+++ packages/hardhat/contracts/YourNFT.sol
@@ -7,9 +7,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+
+import {GelatoRelayContext} from "@gelatonetwork/relay-context/contracts/GelatoRelayContext.sol";
+
 import "./Whitelisting.sol";
 
-contract YourNFT is ERC721URIStorage, Whitelisting {
+contract YourNFT is Whitelisting, GelatoRelayContext, ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -17,16 +20,30 @@ contract YourNFT is ERC721URIStorage, Whitelisting {
 
     constructor() Whitelisting() ERC721("WhitelistToken", "WLT") {}
 
+    function _msgData()
+        internal
+        view
+        virtual
+        override(Context, GelatoRelayContext)
+        returns (bytes calldata)
+    {}
+
     function whitelistMint(
         // bytes32 hash,
         address user,
         uint256 chainId,
         bytes calldata signature,
         string memory tokenURI
-    ) public requiresWhitelist(user, chainId, signature) returns (uint256) {
+    )
+        external
+        onlyGelatoRelay
+        requiresWhitelist(user, chainId, signature)
+        returns (uint256)
+    {
         require(isSignatureUsed[signature] != true, "Signature already used");
 
         uint256 newItemId = _tokenIds.current();
+        _transferRelayFee();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
